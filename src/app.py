ee120be1c0a6805440c2fcfa569861c2db770428
armandott37 <armando_tt23@hotmail.com>
2023-02-15 11:26:11 -0400

    Add routes to /api/v1

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from comment import Comment
 from response import Response
 
 # Function API REST POST create new comment database
-@app.route('/comment_POST', methods=['POST'])
+@app.route('/api/v1/comment', methods=['POST'])
 def create_comment():
     print(request.json)
     params = request.json    
@@ -15,22 +15,22 @@ def create_comment():
     return comment_schema.jsonify(comment)
 
 # Function API REST GET obtain all comments from database
-@app.route('/comments_GET', methods=['GET'])
+@app.route('/api/v1/comments', methods=['GET'])
 def get_comments():    
     comments = Comment.query.all()
     print(comments)    
     return comments_schema.jsonify(comments)
 
 # Function API REST GET obtain comment from database with id parameter
-@app.route('/comment_GET/<int:id_comment>', methods=['GET'])
-def get_comment(id_comment):
-    comment = Comment.query.filter_by(id_comment = id_comment).first()
+@app.route('/api/v1/comment/<int:id>', methods=['GET'])
+def get_comment(id):
+    comment = Comment.query.filter_by(id_comment = id).first()
     return comment_schema.jsonify(comment)
 
 # Function API REST PUT modified comment from database with id parameter
-@app.route('/comment_PUT/<int:id_comment>', methods=['PUT'])
-def put_comment(id_comment):
-    comment = Comment.query.filter_by(id_comment = id_comment).first()
+@app.route('/api/v1/comment/<int:id>', methods=['PUT'])
+def put_comment(id):
+    comment = Comment.query.filter_by(id_comment = id).first()
     params = request.json
     comment.user_comment = params['user_comment']
     comment.datetime_comment = datetime.now()
@@ -41,41 +41,41 @@ def put_comment(id_comment):
     return comment_schema.jsonify(comment)
 
 # Function API REST DELETE delete comment from database with id parameter
-@app.route('/comment_DEL/<int:id_comment>', methods=['DELETE'])
-def delete_comment(id_comment):
-    comment = Comment.query.filter_by(id_comment = id_comment).first()
+@app.route('/api/v1/comment/<int:id>', methods=['DELETE'])
+def delete_comment(id):
+    comment = Comment.query.filter_by(id_comment = id).first()
     db.session.delete(comment)
     db.session.commit()
     print(comment)
     return comment_schema.jsonify(comment)
 
-# Function API REST POST create new response database
-@app.route('/response_POST/<int:id_comment>', methods=['POST'])
-def create_response(id_comment):
+# Function API REST POST create new response database with id comment parameter
+@app.route('/api/v1/response/<int:id>', methods=['POST'])
+def create_response(id):
     print(request.json)
     params = request.json    
-    response = Response(params['user_response'], datetime.now(), params['content_response'], id_comment)
+    response = Response(params['user_response'], datetime.now(), params['content_response'], id)
     db.session.add(response)
     db.session.commit()
     return response_schema.jsonify(response)
 
 # Function API REST GET obtain all responses of comment from database
-@app.route('/responses_GET/<int:id_comment>', methods=['GET'])
-def get_responses(id_comment):    
-    responses = Response.query.filter_by(id_comment = id_comment).all()
+@app.route('/api/v1/responses/<int:id>', methods=['GET'])
+def get_responses(id):    
+    responses = Response.query.filter_by(id_comment = id).all()
     print(responses)    
     return responses_schema.jsonify(responses)
 
-# Function API REST GET obtain response from database with id parameter
-@app.route('/response_GET/<int:id_response>', methods=['GET'])
-def get_response(id_response):
-    response = Response.query.filter_by(id_response = id_response).first()
+# Function API REST GET obtain response from database with id response parameter
+@app.route('/api/v1/response/<int:id>', methods=['GET'])
+def get_response(id):
+    response = Response.query.filter_by(id_response = id).first()
     return response_schema.jsonify(response)
 
 # Function API REST PUT modified response from database with id parameter
-@app.route('/response_PUT/<int:id_response>', methods=['PUT'])
-def put_response(id_response):
-    response = Response.query.filter_by(id_response = id_response).first()
+@app.route('/api/v1/response/<int:id>', methods=['PUT'])
+def put_response(id):
+    response = Response.query.filter_by(id_response = id).first()
     params = request.json
     response.user_response = params['user_response']
     response.datetime_response = datetime.now()    
@@ -85,9 +85,9 @@ def put_response(id_response):
     return response_schema.jsonify(response)
 
 # Function API REST DELETE delete response from database with id parameter
-@app.route('/response_DEL/<int:id_response>', methods=['DELETE'])
-def delete_response(id_response):
-    response = Response.query.filter_by(id_response = id_response).first()
+@app.route('/api/v1/response/<int:id>', methods=['DELETE'])
+def delete_response(id):
+    response = Response.query.filter_by(id_response = id).first()
     db.session.delete(response)
     db.session.commit()
     print(response)
